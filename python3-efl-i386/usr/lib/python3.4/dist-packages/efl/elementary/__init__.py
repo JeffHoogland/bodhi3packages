f8c6b6194213e96bb08d1a1c28bcaac1c914a635
Jeff Hoogland <jeffhoogland@linux.com>
2015-04-07 12:36:36 -0500

    Added python3-efl packages

---
+++ python3-efl-i386/usr/lib/python3.4/dist-packages/efl/elementary/__init__.py
@@ -0,0 +1,95 @@
+# Copyright (C) 2007-2014 various contributors (see AUTHORS)
+#
+# This file is part of Python-EFL.
+#
+# Python-EFL is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 3 of the License, or (at your option) any later version.
+#
+# Python-EFL is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this Python-EFL.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from efl.elementary.general import *
+from efl.elementary.need import *
+
+
+__all__ = (
+    #"access",
+    "actionslider",
+    "background",
+    "box",
+    "bubble",
+    "button",
+    "calendar_elm",
+    "check",
+    "clock",
+    "colorselector",
+    "configuration",
+    "conformant",
+    "ctxpopup",
+    "datetime_elm",
+    "dayselector",
+    "diskselector",
+    "entry",
+    "fileselector",
+    "fileselector_button",
+    "fileselector_entry",
+    "flip",
+    "flipselector",
+    "frame",
+    "general",
+    "gengrid",
+    "genlist",
+    "gesture_layer",
+    #"glview",
+    "grid",
+    "hover",
+    "hoversel",
+    "icon",
+    "image",
+    "index",
+    "innerwindow",
+    "label",
+    "layout_class",
+    "layout",
+    "list",
+    "map",
+    "mapbuf",
+    "menu",
+    "multibuttonentry",
+    "naviframe",
+    "need",
+    "notify",
+    "object",
+    "object_item",
+    "panel",
+    "panes",
+    "photo",
+    "photocam",
+    "plug",
+    "popup",
+    "progressbar",
+    "radio",
+    "scroller",
+    "segment_control",
+    "separator",
+    "slider",
+    "slideshow",
+    "spinner",
+    #"store",
+    "table",
+    "theme",
+    "thumb",
+    "toolbar",
+    "transit",
+    "video",
+    "web",
+    "window",
+)
